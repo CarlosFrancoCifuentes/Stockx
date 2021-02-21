53c8f44b430749172968a1e84c33baaada4e446f
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-02-21 17:09:42 +0100

    addTest Caso test

--- stockx/src/test/java/edu/pingpong/stockx/SneakerTest.java
+++ stockx/src/test/java/edu/pingpong/stockx/SneakerTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import edu.pingpong.stockx.item.Bid;
 import edu.pingpong.stockx.item.Sneaker;
 
 public class SneakerTest {
@@ -11,6 +12,14 @@ public class SneakerTest {
     @Test
     public void toStringTest(){
         Sneaker sneaker = new Sneaker("Estilo", "Nombre");
-        assertEquals("\n\t\t"+ "Nombre" +"\t\t"+ "Estilo", sneaker.toString());
+        assertEquals("\n\t"+ "Nombre" +"\n\t\t"+ "Estilo", sneaker.toString());
+    }
+
+    @Test
+    public void addTest(){
+        Sneaker sneaker = new Sneaker("Estilo", "Nombre");
+        sneaker.add(new Bid("firstSize", 200));
+        sneaker.add(new Bid("secondSize", 100));
+        assertEquals(2, sneaker.offers().size());
     }
 }
