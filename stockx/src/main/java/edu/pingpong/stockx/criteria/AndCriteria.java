60df7145a5eec875ba8723ea802f099cf92974cf
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-02-22 09:43:59 +0100

    Lógica y caso test andCriteria

--- stockx/src/main/java/edu/pingpong/stockx/criteria/AndCriteria.java
+++ stockx/src/main/java/edu/pingpong/stockx/criteria/AndCriteria.java
@@ -2,12 +2,11 @@ package edu.pingpong.stockx.criteria;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import edu.pingpong.stockx.item.Item;
 import edu.pingpong.stockx.item.Offer;
-import edu.pingpong.stockx.item.Sale;
 
 public class AndCriteria implements Criteria{
     
@@ -22,15 +21,12 @@ public class AndCriteria implements Criteria{
     @Override
     public List<Offer> checkCriteria(Item sneaker) {
 
-        Criteria size = new Size(size);
-        Criteria sales = new Sales();
+        List<Offer> firstCriteria = criteria.checkCriteria(sneaker);
+        List<Offer> secondCriteria = otherCriteria.checkCriteria(sneaker);
+        List<Offer> andCriteria = new ArrayList<Offer>();
 
-        List<Offer> firstCriteria = sneaker.offers().stream().filter(n -> n.size().equals())
-            .collect(Collectors.toList());
+        andCriteria = firstCriteria.stream().filter(x -> (secondCriteria.stream().filter(n -> n.equals(x)).count())>=1).collect(Collectors.toList());
 
-        List<Offer> secondCriteria = new ArrayList<Offer>();
-        firstCriteria.stream().filter(s -> s instanceof Sales).forEach(secondCriteria :: add);
-
-        return secondCriteria;
+        return andCriteria;
     }
 }
